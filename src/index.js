d59ec075664a18289908f9e9c60d127aceecc531
nublius <184036331+nublius@users.noreply.github.com>
2025-06-10 12:13:42 -0700

    project-manager

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./styles.css";
 
+import { Item } from "./todo-item.js";
+
 console.log("Hello, World!")
---
+++ src/project.js
@@ -0,0 +1,16 @@
+import { Item } from "./todo-item.js";
+
+export class Project {
+	constructor(title, description, dueDate, priority) {
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.priority = priority;
+		this.toDos = [];
+	}
+
+	addToDo = (title, description, dueDate, priority) => {
+		const toDo = new Item(title, description, dueDate, priority);
+		this.toDos.push(toDo);
+	}
+}
