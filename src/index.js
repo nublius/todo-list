eaf62d5d908a29a28a1f936ebfb5be9971319402
nublius <184036331+nublius@users.noreply.github.com>
2025-06-12 21:21:35 -0700

    redo html and css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import "./styles.css";
 
 import { ProjectManager } from "./project-manager.js";
-
-ProjectManager.addProject("yep", "true", "01/01/2002", "1");
-
-console.log(ProjectManager.showProjects());
