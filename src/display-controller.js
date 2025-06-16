484210664d3fc70272e481001ee61d75dc5e31ac
nublius <184036331+nublius@users.noreply.github.com>
2025-06-16 11:29:47 -0700

    start display controller

---
+++ src/display-controller.js
@@ -0,0 +1,11 @@
+export default const Controller = (function() {
+	const initDisplay = () => {
+		const projectsContainer = document.querySelector(".projects__list");
+
+		const tasksContainer = document.querySelector(".tasks__container");
+
+		return { projectsContainer, tasksContainer };
+	}
+
+
+})();
